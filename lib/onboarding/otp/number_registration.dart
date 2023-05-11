4423e3a28a4eec0a889f6812bb1f9c95f1369326
Jude Enuanwa <lenuanwa@gmail.com>
2023-05-11 22:59:14 +0100

    number_registration

---
+++ lib/onboarding/otp/number_registration.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MobileNumberRegistration extends StatefulWidget {
+  const MobileNumberRegistration({super.key});
+
+  @override
+  State<MobileNumberRegistration> createState() =>
+      _MobileNumberRegistrationState();
+}
+
+class _MobileNumberRegistrationState extends State<MobileNumberRegistration> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
